b2d0c9c829614f7ca2e7ddbe1f14002f1f0159b3
Alessandro <alessandro.boscato@gmail.com>
2020-11-09 17:29:33 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+Route::get('/', 'HomeController@index')->name('home');
+Route::resource('articles', 'ArticleController');
+
+});
+
+// Route::get('/home', 'HomeController@index')->name('home');
